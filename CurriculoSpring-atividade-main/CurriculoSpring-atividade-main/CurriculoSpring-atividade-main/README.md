0bf8b4347f067db3f131655a7a4d53390a20d0a4
pedro-brandt <pedro.2020107111@unicap.br>
2024-11-06 17:06:27 -0300

    removendo pasta

--- CurriculoSpring-atividade-main/CurriculoSpring-atividade-main/CurriculoSpring-atividade-main/README.md
+++
@@ -1,1 +0,0 @@
-### **Curriculo em springboot**
--- CurriculoSpring-atividade-main/CurriculoSpring-atividade-main/CurriculoSpring-atividade-main/src/main/java/AppConfig.java
+++
@@ -1,8 +0,0 @@
-import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-@Configuration
-@EnableTransactionManagement
-public class AppConfig {
-
-}
